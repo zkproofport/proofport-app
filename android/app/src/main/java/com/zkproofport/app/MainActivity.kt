00c9116964bc27c549b2b4ce2dafea71fa8045f8
jaehyuk.hyun <hyuki0130@gmail.com>
2026-01-28 21:31:35 +0900

    add Android 12+ splash screen with dark theme

--- android/app/src/main/java/com/zkproofport/app/MainActivity.kt
+++ android/app/src/main/java/com/zkproofport/app/MainActivity.kt
@@ -1,41 +1,27 @@
 package com.zkproofport.app
-import expo.modules.ReactActivityDelegateWrapper
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import expo.modules.ReactActivityDelegateWrapper
 
 class MainActivity : ReactActivity() {
-
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
   override fun getMainComponentName(): String = "zkProofPort"
 
-  /**
-   * Required for react-native-screens to prevent Fragment restoration crash.
-   * See: https://github.com/software-mansion/react-native-screens/issues/17
-   */
   override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
     super.onCreate(null)
   }
 
-  /**
-   * Handle deep link intents for WalletConnect callback
-   */
   override fun onNewIntent(intent: Intent) {
     super.onNewIntent(intent)
     setIntent(intent)
   }
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled))
 }
